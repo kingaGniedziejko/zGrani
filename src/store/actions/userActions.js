28c9e1ff5d3ab9a4a16e79a02476c4125f19edbd
kingaGniedziejko <47779092+kingaGniedziejko@users.noreply.github.com>
2020-10-05 19:15:25 +0200

    redux-firestore connection

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -1,7 +1,16 @@
 export const createUser = (user) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async call to database
-        dispatch({type: 'CREATE_USER', user});
+        const firestore = getFirestore();
+
+        firestore.collection('users').add({
+            ...user,
+            signupDate: new Date(),
+            type: 'test'
+        })
+            .then(() => dispatch({ type: 'CREATE_USER', user }))
+            .catch((err) => dispatch({ type: 'CREATE_USER_ERROR', err }))
+
     }
 
 }
--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -3,9 +3,14 @@ const initState = {};
 const userReducer = (state = initState, action) => {
     switch (action.type) {
         case 'CREATE_USER':
-            console.log('created user', action.user)
+            console.log('created user', action.user);
+            return state;
+        case 'CREATE_USER_ERROR':
+            console.log('create user error', action.err)
+            return state;
+        default:
+            return state;
     }
-    return state;
 }
 
 export default userReducer;
