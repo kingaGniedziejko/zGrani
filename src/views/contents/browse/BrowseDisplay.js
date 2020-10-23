bec60e841901d920cce6cfa1278f70f0d2fa4c3e
kingaGniedziejko <47779092+kingaGniedziejko@users.noreply.github.com>
2020-10-23 11:03:35 +0200

    browse with limit

--- src/views/contents/browse/BrowseDisplay.js
+++ src/views/contents/browse/BrowseDisplay.js
@@ -5,20 +5,44 @@ import {ChevronDown} from "react-bootstrap-icons";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
+import Loader from "../../layouts/Loader";
 
 class BrowseDisplay extends Component{
+    state = {
+        limit: 16
+    }
+
+    handleExtend = () => {
+        const limitBy = 16;
+        this.setState({
+            limit: this.state.limit + limitBy
+        })
+    }
+
     render() {
         let { users, instrument, genre } = this.props;
+        const { limit } = this.state;
+
+        if (!users) return <Loader/>
 
         if (instrument && genre) {
             users = users.filter(user => user.genresId.includes(genre.id));
+        }
+        
+        let usersList = [];
+        let isMore = users.length > limit;
+
+        if (isMore) {
+            usersList = users.slice(0, limit);
+        } else {
+            usersList = users;
         }
 
         return (
             <div className={"section d-flex flex-column align-items-center"}>
                 <Container>
                     <Row>
-                        {users && users.map((user, index) => {
+                        {usersList && usersList.map((user, index) => {
                             if (user) {
                                 return (
                                     <Col key={index} sm={6} lg={3}><ProfileShortcut user={user}/></Col>
@@ -29,10 +53,13 @@ class BrowseDisplay extends Component{
                         })}
                     </Row>
                 </Container>
-                <div className={"d-flex flex-column align-items-center clickable"}>
-                    <p className={"m-0"}>Więcej</p>
-                    <ChevronDown/>
-                </div>
+                {isMore ?
+                    <div className={"d-flex flex-column align-items-center clickable"} onClick={this.handleExtend}>
+                        <p className={"m-0"}>Więcej</p>
+                        <ChevronDown/>
+                    </div>
+                    : ""
+                }
             </div>
         );
     }
